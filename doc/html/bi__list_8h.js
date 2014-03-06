1ace62d4f5285a1dc2d8f6030a84fdda2a156bfe
norethel <ciesldaw@gmail.com>
2014-03-06 17:22:01 +0100

    doxygen documentation updated

--- doc/html/bi__list_8h.js
+++ doc/html/bi__list_8h.js
@@ -4,9 +4,16 @@ var bi__list_8h =
     [ "TBiList", "bi__list_8h.html#a022731eadf7426e98e6b164fdc3369c2", null ],
     [ "bi_list_back", "bi__list_8h.html#a14cc816069c35df41fcb1ad9ea100675", null ],
     [ "bi_list_cctor", "bi__list_8h.html#a7843f5bdd9569b29c03ba82b02d8253e", null ],
+    [ "bi_list_clear", "bi__list_8h.html#aed8a5d9c37e4280558f3d327b38efaee", null ],
     [ "bi_list_ctor", "bi__list_8h.html#ac05bb3c41e5bc0036f327ef75b4e8c6f", null ],
     [ "bi_list_dtor", "bi__list_8h.html#ac25532cc7f8e6d702dffcb8a1cf8e409", null ],
     [ "bi_list_empty", "bi__list_8h.html#ae365192d392f36ccde6cdae587735c23", null ],
+    [ "bi_list_front", "bi__list_8h.html#ab0a2a5fc31b35e13fbbd392e54fcb8e5", null ],
+    [ "bi_list_insert", "bi__list_8h.html#adc009cfa5d0cedad2a660e4517210e2c", null ],
     [ "bi_list_pop_back", "bi__list_8h.html#a53d52e0166f2ca140e93b23256ea2e2f", null ],
-    [ "bi_list_push_back", "bi__list_8h.html#aa53e556e8aefaf5d1edcb5c74290b971", null ]
+    [ "bi_list_pop_front", "bi__list_8h.html#ac8046b8f8c82447dfdc9f0155ab83748", null ],
+    [ "bi_list_push_back", "bi__list_8h.html#aa53e556e8aefaf5d1edcb5c74290b971", null ],
+    [ "bi_list_push_front", "bi__list_8h.html#a5acde327f5ab065a9ef5ceed472eb54c", null ],
+    [ "bi_list_remove", "bi__list_8h.html#a0fdbf1b372b53f1e48a038b9d5af8192", null ],
+    [ "bi_list_size", "bi__list_8h.html#ac97fe45de184c9c434881c20fafa68c1", null ]
 ];
--- doc/html/search/all_62.js
+++ doc/html/search/all_62.js
@@ -12,9 +12,16 @@ var searchData=
   ['bi_5flist_2eh',['bi_list.h',['../bi__list_8h.html',1,'']]],
   ['bi_5flist_5fback',['bi_list_back',['../bi__list_8h.html#a14cc816069c35df41fcb1ad9ea100675',1,'bi_list.h']]],
   ['bi_5flist_5fcctor',['bi_list_cctor',['../bi__list_8h.html#a7843f5bdd9569b29c03ba82b02d8253e',1,'bi_list.h']]],
+  ['bi_5flist_5fclear',['bi_list_clear',['../bi__list_8h.html#aed8a5d9c37e4280558f3d327b38efaee',1,'bi_list.h']]],
   ['bi_5flist_5fctor',['bi_list_ctor',['../bi__list_8h.html#ac05bb3c41e5bc0036f327ef75b4e8c6f',1,'bi_list.h']]],
   ['bi_5flist_5fdtor',['bi_list_dtor',['../bi__list_8h.html#ac25532cc7f8e6d702dffcb8a1cf8e409',1,'bi_list.h']]],
   ['bi_5flist_5fempty',['bi_list_empty',['../bi__list_8h.html#ae365192d392f36ccde6cdae587735c23',1,'bi_list.h']]],
+  ['bi_5flist_5ffront',['bi_list_front',['../bi__list_8h.html#ab0a2a5fc31b35e13fbbd392e54fcb8e5',1,'bi_list.h']]],
+  ['bi_5flist_5finsert',['bi_list_insert',['../bi__list_8h.html#adc009cfa5d0cedad2a660e4517210e2c',1,'bi_list.h']]],
   ['bi_5flist_5fpop_5fback',['bi_list_pop_back',['../bi__list_8h.html#a53d52e0166f2ca140e93b23256ea2e2f',1,'bi_list.h']]],
-  ['bi_5flist_5fpush_5fback',['bi_list_push_back',['../bi__list_8h.html#aa53e556e8aefaf5d1edcb5c74290b971',1,'bi_list.h']]]
+  ['bi_5flist_5fpop_5ffront',['bi_list_pop_front',['../bi__list_8h.html#ac8046b8f8c82447dfdc9f0155ab83748',1,'bi_list.h']]],
+  ['bi_5flist_5fpush_5fback',['bi_list_push_back',['../bi__list_8h.html#aa53e556e8aefaf5d1edcb5c74290b971',1,'bi_list.h']]],
+  ['bi_5flist_5fpush_5ffront',['bi_list_push_front',['../bi__list_8h.html#a5acde327f5ab065a9ef5ceed472eb54c',1,'bi_list.h']]],
+  ['bi_5flist_5fremove',['bi_list_remove',['../bi__list_8h.html#a0fdbf1b372b53f1e48a038b9d5af8192',1,'bi_list.h']]],
+  ['bi_5flist_5fsize',['bi_list_size',['../bi__list_8h.html#ac97fe45de184c9c434881c20fafa68c1',1,'bi_list.h']]]
 ];
